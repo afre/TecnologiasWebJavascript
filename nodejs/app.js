10b8de3264c7188a0d12972f7657697fc6f08dcd
vengatus <juanque@outlook.com>
2016-11-23 14:37:03 -0500

    paquete version 1.0.01

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,4 +1,22 @@
+//tecnologiaswebfalex.imprimirSuma()
+
+module.exports = {
+    imprimirSuma: imprimirSuma,
+ // customField: 'Custom Field',
+  //otherCustomField: 'This field is really cool'
+}
+/*
+tecnologiaswebfalex.imprimirSuma
+
+module.exports = {
+    imprimirSuma: imprimirSuma(),
+  //customField: 'Custom Field',
+  //otherCustomField: 'This field is really cool'
+}
+*/
+function imprimirSuma(){
 var n1=1;
 n2=2;
 
 console.log(n1+n2);
+}
