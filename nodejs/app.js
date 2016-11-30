c01187a3d907e17c5261d73755637c2ee47f0be1
afre <a.frex_b@hotmail.com>
2016-11-30 14:21:35 -0500

    commit parcial

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var app = express();
+var puerto=3030;
+
+var usuarios=[{
+    id:1,
+    nombre:'alex',
+    cedula:'9999'
+},{
+    id:2,
+    nombre:'alex2',
+    cedula:'99992'
+},
+             id:3,
+    nombre:'alex2',
+    cedula:'99993'];
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/tecnologiasWebJavascript1', function (req, res) {
+  res.send('get con javascript');
 });
+
+app.post('/tecnologiasWebJavascript2', function (req, res) {
+    //var parametro=req.params;
+  res.send('post con javascript');
+});
+
+app.listen(puerto, function () {
+  console.log('Example app listening on port'+puerto+'!');
+    console.log('Cabecera req');
+    //console.log(req.headers);
+    console.log('Cabecera res');
+    //console.log(res.headers);
+    //res.json(usuario);
+              //console.log("Cabeceras del response");
+              //console.log("Cabeceras del response");
+//aumentar campos en un objeto json
+    //usuario.apellido='';
+    
+    var usuario={nombre:'alex',
+                cedula:1766666666};
+    
+});
+
