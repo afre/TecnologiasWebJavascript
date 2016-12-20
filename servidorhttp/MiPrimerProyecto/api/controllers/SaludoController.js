f7274bef0c7061ad20c2c437ca6c12619197e4e4
a <a>
2016-12-20 15:12:05 -0500

    transporte todavía por corregir

--- servidorhttp/MiPrimerProyecto/api/controllers/SaludoController.js
+++ servidorhttp/MiPrimerProyecto/api/controllers/SaludoController.js
@@ -1,3 +1,28 @@
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
+    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plaintext body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+/*
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
+*/
+
+
 /**
  * SaludoController
  *
@@ -8,8 +33,23 @@
 module.exports = {
 	hola:function(req,res){
         
+        var values = req.allParams();
+        
         if(req.method=='GET'){
-    res.json({a:'1','b':'Hola get'});            
+            
+            
+        transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+            
+            res.send('ok');
+});    
+            
+            
+    res.json({a:'1','b':'Hola get',
+             para:values});            
         }
         else if(req.method=='POST'){
     res.json({a:'2','b':'Hola post'});            
