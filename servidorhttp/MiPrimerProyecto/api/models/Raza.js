97c4978842d0cc380d65f1045cbd269ef20e53b5
afre <a.frex_b@hotmail.com>
2017-01-10 14:29:46 -0500

    sails

--- servidorhttp/MiPrimerProyecto/api/models/Raza.js
+++ servidorhttp/MiPrimerProyecto/api/models/Raza.js
@@ -1,5 +1,5 @@
 /**
- * Usuario.js
+ * Raza.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
@@ -9,21 +9,6 @@ module.exports = {
 
   attributes: {
 
-nombre:{
-    type:'string',
-    minLength:5
-    required:true
-},
-  apellidos:{
-    type:'string',
-      minLength:5,
-      required:true
-},
-     correo:{
-    type:'string',
-      email:true,
-         defaultsTo:'correo@invalido.com'
-}
   }
 };
 
